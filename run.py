7bee2eeac6e28dd02cccb184d674a4c4d2d89d4f
Louis Mozart KAMDEM TEYOU <93260304+Louis-Mozart@users.noreply.github.com>
2025-10-02 15:40:09 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from dicee.executer import Execute, ContinuousExecute
 import argparse
 
 def get_default_arguments(description=None):
-    """ Extends pytorch_lightning Trainer's arguments with ours """
+    """ Extends pytorch_lightning Trainer's arguments with ours. """
     # From "pytorch-lightning==1.6.4" to "lightning>=2.1.3",  'Trainer' has no attribute 'add_argparse_args'
     # parser = pl.Trainer.add_argparse_args(argparse.ArgumentParser(add_help=False))
     parser = argparse.ArgumentParser(add_help=False)
